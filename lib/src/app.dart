c72e0b27590274718e18ea8cd85bd5cb59e4bafe
Shane <shanewilsonpro@gmail.com>
2020-05-05 02:25:29 -0700

    add bottom navigation bar and new pages

--- lib/src/app.dart
+++ lib/src/app.dart
@@ -1,6 +1,6 @@
   
 import 'package:flutter/material.dart';
-import 'homescreen.dart';
+import 'screens/main_screen.dart';
 
 class App extends StatelessWidget {
 
@@ -9,7 +9,7 @@ class App extends StatelessWidget {
     return MaterialApp(
         title: "Food Services App",
         theme: ThemeData(primaryColor: Colors.blueAccent),
-        home: HomeScreen(),
+        home: MainScreen(),
       );
   }
 }
